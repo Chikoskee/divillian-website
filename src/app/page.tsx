375434e9f1ef503bc31214d8c16457209228f264
Papo <robertvtemployee@gmail.com>
2026-07-31 00:16:18 +0800

    Remove duplicated phrase from About section text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default async function Home() {
         <h2 className="section-title">About Divil&apos;Lian</h2>
         <div className="about-text">
           <p className="about-lead">
-            Doing what you got to do to survive, for those who look over the skies but over looked by some eyes but are overlooked by some eyes. Born in New Jersey, the little big brother of the east, for those trying to scrap for a plate at the feast! Divil&apos;Lians.
+            Doing what you got to do to survive, for those who look over the skies but are overlooked by some eyes. Born in New Jersey, the little big brother of the east, for those trying to scrap for a plate at the feast! Divil&apos;Lians.
           </p>
         </div>
       </div>
